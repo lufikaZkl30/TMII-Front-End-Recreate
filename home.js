db94b225a289dfd93315a20125d4dda0dc74d8d1
lufikaZkl30 <149558662+lufikaZkl30@users.noreply.github.com>
2023-12-07 22:04:47 +0700

    update

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 // button on to halaman baru
 document.getElementById('btn-explore').addEventListener('click', function() {
-    window.location.href = 'index.html';
+    window.location.href = 'tiket.html';
 });
